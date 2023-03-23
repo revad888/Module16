ebb8bfd837c29ea9f1e750bfc911e9c2a21a2b42
Revenko Vladislav <revad8@gmail.com>
2023-03-23 22:13:39 +0300

    Добавьте файлы проекта.

---
+++ Module16.Tests/CalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Module16.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator calculator = new Calculator();
+        [Test]
+        public void AdditionMustustReturnCorrectValuel()
+        {
+            Assert.AreEqual(8, calculator.Additional(4, 4));
+        }
+
+        [Test]
+        public void AdditionMustustReturnNotNull()
+        {
+            Assert.IsNotNull(calculator.Additional(4, 4));
+        }
+
+        [Test]
+        public void SubtractionMustustReturnCorrectValuel()
+        {
+            Assert.AreEqual(-8, calculator.Subtraction(-4, 4));
+        }
+
+        [Test]
+        public void SubtractionMustustReturnNotNulll()
+        {
+            Assert.NotNull(calculator.Subtraction(0, 0));
+        }
+
+
+        [Test]
+        public void MiltiplicationMustustReturnCorrectValuel()
+        {
+            Assert.AreEqual(-8, calculator.Miltiplication(4, -2));
+        }
+
+        public void MiltiplicationMustustReturnNotNulll()
+        {
+            Assert.NotNull(calculator.Miltiplication(0, 0));
+        }
+
+
+        [Test]
+        public void DivisionMustustReturnCorrectValuel()
+        {
+            Assert.AreEqual(-1, calculator.Division(4, -4));
+        }
+
+        [Test]
+        public void DivisionMustustReturnExrption()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(4, 0));
+        }
+
+        [Test]
+        public void DivisionMustustReturnNotNulll()
+        {
+            Assert.NotNull(calculator.Division(0, 1));
+        }
+    }
+}
